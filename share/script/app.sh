e3222c739ed52231b77a57e3112086e51b8f2185
Hong, Dewey <doohee323@gmail.com>
2016-07-16 10:13:42 -0700

    init

--- share/script/app.sh
+++ share/script/app.sh
@@ -2,6 +2,51 @@
 
 set -x
 
-touch app
+cd /home/core/share/service
+
+echo "##[test etcdctl]####################################"
+etcdctl ls
+etcdctl ls --recursive
+etcdctl set /test hello
+etcdctl get /test
+etcdctl rm /test
+etcdctl get /test
+etcdctl watch /test &
+etcdctl set /test hello --ttl 10
+etcdctl get /test
+etcdctl set /_test hello   # hidden
+etcdctl get /_test
+
+echo "##[test systemctl]####################################"
+#systemctl start app.1.service
+#journalctl -u app.1.service #-f
+#docker ps -a
+
+#systemctl stop app.1.service
+#docker ps -a
+
+echo "##[test service with fleet]####################################"
+fleetctl start app.1.service
+fleetctl start app.2.service
+# fleetctl start app*
+
+docker ps -a
+fleetctl list-units
+fleetctl journal app.1.service # fleetctl journal -f app.1.service
+fleetctl stop app*
+
+echo "##[test service with fleet template]####################################"
+fleetctl submit app@.service
+fleetctl list-unit-files
+fleetctl start app@{1..2}.service
+fleetctl list-units
+fleetctl journal app@1.service
+
+echo "##[test discovery with fleet]####################################"
+fleetctl start web.service
+fleetctl load web-discovery.service  # only save
+fleetctl start web-discovery.service
+etcdctl get /services/web/nginx
+fleetctl cat web-discovery.service
 
 exit 0
--- share/script/init.sh
+++ share/script/init.sh
@@ -8,7 +8,24 @@ echo 'alias "ll=ls -al"' >> /home/core/.profile
 cd /home/core
 source .profile
 
-cd share/script
-bash app.sh
+# install shipyard
+#curl -sSL https://shipyard-project.com/deploy | bash -s
+
+ip addr show | grep 'inet 172.'
+
+ssh-keygen -b 4096 -t rsa -f ~/.ssh/id_rsa -q -N ""
+ssh-add ~/.ssh/id_rsa
+ID_RSD=/home/core/share/resources/$HOSTNAME
+cat ~/.ssh/id_rsa.pub > $ID_RSD
+
+if [ "$HOSTNAME" = "core-02" ]; then
+	cat /home/core/share/resources/core-01 >> ~/.ssh/authorized_keys
+fi
+
+if [ "$HOSTNAME" = "core-01" ]; then
+	cat /home/core/share/resources/core-02 >> ~/.ssh/authorized_keys
+fi
+
+bash /home/core/share/script/app.sh
 
 exit 0

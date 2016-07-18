1dbcc9e3c2c035227e03887c65e440bb345fbbb1
Hong, Dewey <doohee323@gmail.com>
2016-07-17 21:15:50 -0700

    add some test cases

--- share/script/app.sh
+++ share/script/app.sh
@@ -2,12 +2,10 @@
 
 set -x
 
-cd /home/core/share/service
-
 ### [update certs] ############################################################################################################
-sudo cp /vagrant/domain.crt /usr/share/ca-certificates/
-echo "domain.crt" | sudo tee -a /etc/ca-certificates.conf
-sudo update-ca-certificates
+sudo cp /home/core/share/resources/domain.pem /etc/ssl/certs/domain.pem
+#echo "domain.crt" | sudo tee -a /etc/ca-certificates.conf
+sudo update-ca-certificates | grep domain
 
 ### [pull test image from external server with https] ##########################################################################
 sudo docker login --username=testuser --password=testpassword https://registry.tz.com:5000
--- share/script/test.sh
+++ share/script/test.sh
@@ -12,17 +12,28 @@ etcdctl get /test
 etcdctl rm /test
 etcdctl get /test
 etcdctl watch /test &
+#etcdctl -o extended watch /test &  # debugging
 etcdctl set /test hello --ttl 10
 etcdctl get /test
 etcdctl set /_test hello   # hidden
 etcdctl get /_test
 
-echo "##[test systemctl]####################################"
-#systemctl start app.1.service
-#journalctl -u app.1.service #-f
-#docker ps -a
+etcdctl exec-watch --recursive /test -- sh -c "env | grep ETCD"
+etcdctl set /test/bar 1
 
-#systemctl stop app.1.service
+echo "##[test systemctl]####################################"
+sudo su
+cd /etc/systemd/system
+cp /home/core/share/service/hello.service .
+systemctl enable hello.service
+systemctl disable hello.service
+systemctl enable hello.service
+systemctl start hello.service
+#systemctl status hello.service
+#journalctl -u hello.service #-f
+docker ps -a
+exit
+#systemctl stop hello.service
 #docker ps -a
 
 echo "##[test service with fleet]####################################"
@@ -34,6 +45,13 @@ docker ps -a
 fleetctl list-units
 fleetctl journal app.1.service # fleetctl journal -f app.1.service
 fleetctl stop app*
+fleetctl destroy app*
+
+echo "##[test global service with fleet]####################################"
+fleetctl start global.service
+docker ps -a
+fleetctl list-units
+fleetctl stop global.service
 
 echo "##[test service with fleet template]####################################"
 fleetctl submit app@.service
@@ -48,5 +66,21 @@ fleetctl load web-discovery.service  # only save
 fleetctl start web-discovery.service
 etcdctl get /services/web/nginx
 fleetctl cat web-discovery.service
+
+echo "##[system monitoring]####################################"
+systemctl status -l fleet
+systemctl status -l etcd
+journalctl -b -u etcd
+
+vi .toolboxrc
+TOOLBOX_DOCKER_IMAGE=debian
+TOOLBOX_DOCKER_TAG=jessie
+TOOLBOX_DOCKER_USER=root
+toolbox
+
+
+
+
+
 
 exit 0
